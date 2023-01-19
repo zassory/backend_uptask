eed3600f04ed516474421218286ab523a3b67072
zassory <nicolas.programador@gmail.com>
2023-01-19 19:07:28 -0300

    endpoint nuevo de olvide password, enviando instrucciones y generando nuevo token con generarId

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -73,8 +73,27 @@ const confirm = async (req, res = response) => {
     }
 };
 
+const olvidePassword = async (req, res = response) => {
+    const { email } = req.body;
+    const usuario = await Usuario.findOne({ email });
+    if(!usuario){
+        const error = new Error("El usuario no existe");
+        return res.status(404).json({msg: error.message});
+    }
+
+    try{
+        usuario.token = generarId();
+        await usuario.save();
+        res.json({msg: "Hemos enviado un email con las instrucciones"});
+    }catch(error){
+        console.log(error);
+    }
+    
+};
+
 export {
     register,
     authenticate,
     confirm,
+    olvidePassword,
 };
--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 const router = express.Router();//Esto lo da express
-import { register , authenticate , confirm } from "../controllers/usuarioController.js";
+import { register , authenticate , confirm , olvidePassword } from "../controllers/usuarioController.js";
 
 //Autenticated,Create, Add and Confirm users
 router.post("/",register);
 router.post("/login",authenticate);
-router.get('/confirmar/:token', confirm);
+router.get("/confirmar/:token", confirm);
+router.post("/olvide-password", olvidePassword);
 
 export default router;

d804ebde3e58b5c3291e0b6703b90877eb75ad38
zassory <nicolas.programador@gmail.com>
2023-01-24 20:47:11 -0300

    rutas y controladores listos para el desarrollo

---
+++ controllers/proyectoController.js
@@ -0,0 +1,44 @@
+import { request , response } from "express";
+
+const obtenerProyectos = async(req = request,res = response) => {
+
+}
+
+const nuevoProyecto = async(req = request,res = response) => {
+
+}
+
+const obtenerProyecto = async(req = request,res = response) => {
+
+}
+
+const editarProyecto = async(req = request,res = response) => {
+
+}
+
+const eliminarProyecto = async(req = request,res = response) => {
+
+}
+
+const agregarColaborador = async(req = request,res = response) => {
+
+}
+
+const eliminarColaborador = async(req = request,res = response) => {
+
+}
+
+const obtenerTareas = async(req = request,res = response) => {
+
+}
+
+export {
+    obtenerProyectos,
+    nuevoProyecto,
+    obtenerProyecto,
+    editarProyecto,
+    eliminarProyecto,
+    agregarColaborador,
+    eliminarColaborador,
+    obtenerTareas,
+}

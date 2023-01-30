870053faf1cead3e492cf957ad6d8b8828105a45
zassory <nicolas.programador@gmail.com>
2023-01-30 19:39:03 -0300

    controlador cascaron ok, rutas listas

---
+++ controllers/tareaController.js
@@ -0,0 +1,29 @@
+import { response , request } from "express";
+
+const agregarTarea = async(req = request , res = response ) => {
+
+}
+
+const obtenerTarea = async(req = request , res = response ) => {
+    
+}
+
+const actualizarTarea = async(req = request , res = response ) => {
+    
+}
+
+const eliminarTarea = async(req = request , res = response ) => {
+    
+}
+
+const cambiarEstado = async(req = request , res = response ) => {
+    
+}
+
+export {
+    agregarTarea,
+    obtenerTarea,
+    actualizarTarea,
+    eliminarTarea,
+    cambiarEstado,
+}

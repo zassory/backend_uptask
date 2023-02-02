cee1a8671fb48f74bb3532203775893cd4c9b1ce
zassory <nicolas.programador@gmail.com>
2023-02-02 19:02:22 -0300

    Envio de correo con nodemailer ok

--- helpers/email.js
+++ helpers/email.js
@@ -1,6 +1,32 @@
 import nodemailer from "nodemailer";
 
-export const emailRegistro = (datos) => {
+export const emailRegistro = async(datos) => {
 
-    console.log("DATOS",datos);
+    const { email , nombre , token } = datos;
+
+    const transport = nodemailer.createTransport({
+        host: "sandbox.smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+          user: "d08c6f4915c453",
+          pass: "19b41cde18fb06"
+        }
+    });
+
+    //Información del email
+    const info = await transport.sendMail({
+        from: '"UpTask - Administrador de Proyectos" <cuentas@uptask.com>',
+        to: email,
+        subject: "UpTask - Comprueba tu cuenta",
+        text: "Comprueba tu cuenta en UpTask",
+        html: `<p>Hola: ${nombre} Comprueba tu cuenta en UpTask</p>
+        <p>Tu cuenta ya esta casi lista, solo debes comprobarla en el siguiente enlace:
+
+        <a href="${process.env.FRONTEND_URL}/confirmar/${token}">Comprobar Cuenta</a>
+
+        <p>Si tu no creaste esta cuenta, puedes ignorar el mensaje</p>
+        
+        `
+    })
+
 };

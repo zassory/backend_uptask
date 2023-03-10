5127f2ab62c9e58a3845aeeb1477c0c69f79fc6a
zassory <nicolas.programador@gmail.com>
2023-03-10 18:14:24 -0300

    socket creado y room ok

--- index.js
+++ index.js
@@ -55,11 +55,19 @@ const io = new Server(servidor, {
 io.on("connection", (socket) => {
     console.log("Conectado a socket.io");
 
-    //Definir los eventos de socket io
-    socket.on('prueba', (proyectos)=> {
-        console.log('Prueba desde Socket io ', proyectos);
+   //Definir los eventos de socket io
+   socket.on("abrir proyecto", (proyecto) => {
+        //Saber en que proyecto estoy me permite entrar en este cuarto
+        //Entro a un cuarto
+        socket.join(proyecto);
 
-        socket.emit("respuesta",{ nombre: "Nicolas" });
-    })
+        
+
+
+        //El servidor le esta enviando una respuesta a todos los 
+        //usuarios que esten conectados
+        //A todos los que abrieron una conexion con socket io
+        //socket.to("63eaa6a496ca8776d4a371c9").emit('respuesta', {nombre:'Nicolas'});
+   });
 
 });

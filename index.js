b61b260d2b2fe21c39e595d9cbfd0f5b1e0df144
zassory <nicolas.programador@gmail.com>
2023-03-10 16:33:15 -0300

    uso en index.js de socket io y agregando la variable servidor toda mi conexion

--- index.js
+++ index.js
@@ -38,6 +38,16 @@ app.use("/api/tareas", tareaRoutes);
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
+const servidor = app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
-})
+});
+
+// Socket.io
+import {} from 'socket.io';
+
+const io = new Server(servidor, {
+    pingTimeout: 60000,
+    cors: {
+        origin: process.env.FRONTEND_URL,
+    },
+});

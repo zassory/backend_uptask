8302196db6a8a84e857324555cad85630815e9d5
zassory <nicolas.programador@gmail.com>
2023-03-10 17:12:10 -0300

    recibiendo datos con on en mi back

--- index.js
+++ index.js
@@ -56,5 +56,8 @@ io.on("connection", (socket) => {
     console.log("Conectado a socket.io");
 
     //Definir los eventos de socket io
+    socket.on('prueba', (proyectos)=> {
+        console.log('Prueba desde Socket io ', proyectos);
+    })
 
-})
+});
